e10025c36e36a1b39490b21f319ca97b6de1c5e9
Diego Cabrera <diego.cabrera.15@outlook.com>
2024-03-03 02:02:10 -0500

    [PAY-001]Fix error en method GetItemByFilterCore and add new params

--- dynamodbcore/dynamodb_operations.go
+++ dynamodbcore/dynamodb_operations.go
@@ -134,9 +134,10 @@ func (d DynamoDBRepository) GetItemByFieldCore(ctx context.Context, request even
 	input := &dynamodb.QueryInput{
 		IndexName:              aws.String(globalSecondaryIndex),
 		TableName:              aws.String(d.table),
-		KeyConditionExpression: aws.String(fieldNameFilterByID + " = :" + fieldNameFilterByID),
+		KeyConditionExpression: aws.String(fieldNameFilterByID + " = :" + fieldNameFilterByID + " and " + fieldNameFilterStatus + " = :" + fieldNameFilterStatus),
 		ExpressionAttributeValues: map[string]types.AttributeValue{
-			":" + fieldNameFilterByID: &types.AttributeValueMemberS{Value: fieldValueFilterByID},
+			":" + fieldNameFilterByID:   &types.AttributeValueMemberS{Value: fieldValueFilterByID},
+			":" + fieldNameFilterStatus: &types.AttributeValueMemberS{Value: fieldValueFilterStatus},
 		},
 	}
 	logs.LogTrackingInfoData("GetItemByFieldCore input", input, ctx, request)
